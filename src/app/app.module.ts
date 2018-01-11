aa9babb425c8db3f66f348769cca14d9cf88f22d
Joseph Bir <joe.bir.code@gmail.com>
2018-01-11 14:56:27 -0500

    Setup dialog page, added home page, added 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { MatToolbarModule,
   MatButtonModule,
  } from '@angular/material';
@@ -7,15 +9,27 @@ import { MatToolbarModule,
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DialogbaseComponent } from './components/dialogbase/dialogbase.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dialog', component: DialogbaseComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DialogbaseComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     MatToolbarModule,
     MatButtonModule,
   ],
