38dbab0bdc6fc12336ac280493505ec79ebff0eb
Joseph Bir <joe.bir.code@gmail.com>
2018-01-11 16:58:37 -0500

    Cut Dialogbase, check Slack for Jeremy's updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule,
+import { 
+  MatToolbarModule,
   MatButtonModule,
+  MatDialogModule,
  } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { DialogbaseComponent } from './components/dialogbase/dialogbase.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes = [
   { path: '', component: HomeComponent },
-  { path: 'dialog', component: DialogbaseComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
@@ -23,15 +24,16 @@ const routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    DialogbaseComponent,
     HomeComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
